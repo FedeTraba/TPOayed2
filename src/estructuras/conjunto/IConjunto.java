53205f872fd41a7fe029a452cf58b9797070f2f3
elcostann <nicocostantini@gmail.com>
2023-05-04 00:29:42 -0300

    Implementacion conjunto

--- src/estructuras/conjunto/IConjunto.java
+++ src/estructuras/conjunto/IConjunto.java
@@ -1,5 +1,12 @@
 package estructuras.conjunto;
+import snippets.productoController.Producto;
 
 public interface IConjunto {
+    void inicializarConjuto();
+    void agregar(Producto x);
+    Producto elegir();
+    boolean conjuntoVacio();
+    void sacar(Producto x);
+    boolean pertenece(Producto x);
 
 }
--- src/estructuras/conjunto/implementacion/Conjunto.java
+++ src/estructuras/conjunto/implementacion/Conjunto.java
@@ -1,4 +1,62 @@
 package estructuras.conjunto.implementacion;
 
-public class Conjunto {
+import estructuras.conjunto.IConjunto;
+import snippets.productoController.Producto;
+
+public class Conjunto implements IConjunto {
+    private class Nodo{
+       Producto info;
+       Nodo sig;
+    }
+    private Nodo c;
+
+
+    public void inicializarConjuto() {
+        c = null;
+
+    }
+
+
+    public void agregar(Producto x) {
+        if (!this.Pertenece(x)) {
+            Nodo nuevo = new Nodo();
+            nuevo.info = x;
+            nuevo.sig = c;
+            c = nuevo;
+        }
+
+    }
+
+
+    public Producto elegir() {
+        return c.info;
+    }
+
+
+    public boolean conjuntoVacio() {
+        return c == null;
+    }
+
+
+    public void sacar(Producto x) {
+    if (c !=null) {
+        if(c.info == x){
+            c = c.sig;
+        } else {
+            Nodo aux = c;
+            while (aux.sig != null && aux.sig.info != x)
+                aux = aux.sig;
+            if (aux.sig != null) {
+                aux.sig = aux.sig.sig;
+            }
+        }
+    }
+    }
+
+    public boolean pertenece(Producto x) {
+        Nodo aux = c;
+        while (aux != null && aux.info != x)
+            aux = aux.sig;
+        return (aux != null);
+    }
 }
